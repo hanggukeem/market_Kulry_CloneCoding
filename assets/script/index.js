1a881f356eb38ea138a3eec7f45b907a5e1f83a4
hanggukeem <63224872+hanggukeem@users.noreply.github.com>
2020-10-21 20:12:36 +0900

    mainSlideshow 수정

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,11 +1,49 @@
 
 // main slideshow
-setInterval(fnSlide, 3000);
+var intervalRef = setInterval(fnSlide, 3000);
 
 function fnSlide() {
     $("#slideFrame_main").animate({ "margin-left": "-100%" }, 500, function () {
         $(this).css({ "margin-left": "0" });
         $("#slideFrame_main>a:first").insertAfter("#slideFrame_main>a:last");
     });
+
 }
 
+//slide Btn 활성화
+$("#mainSlide").mouseover(function () {
+    $("div#slideBtn > button").css("display", "block");
+});
+$("#mainSlide").mouseout(function () {
+    $("div#slideBtn > button").css("display", "none");
+});
+
+//button에 mouseover하면 슬라이드 멈춤
+$("div#slideBtn > button").mouseover(function () {
+    clearInterval(intervalRef);
+    $(this).mouseout(function () {
+        intervalRef = setInterval(fnSlide, 2000);
+    });
+});
+$("div#slideBtn > button").mouseout(function () {
+    intervalRef = setInterval(fnSlide, 2000);
+    $this.mouseover(function () {
+        clearInterval(intervalRef);
+    });
+});
+
+//클릭하면 이동
+$("button#slide_left").click(function (e) {
+    $("#slideFrame_main").animate({ "margin-left": "-100%" }, 500, function () {
+        $(this).css({ "margin-left": "0" });
+        $("#slideFrame_main>a:first").insertAfter("#slideFrame_main>a:last");
+    })
+    e.stopPropagation();
+});
+
+$("button#slide_right").click(function () {
+    $("#slideFrame_main").animate({ "margin-right": "-100%" }, 500, function () {
+        $(this).css({ "margin-right": "0" });
+        $("#slideFrame_main>a:first").insertAfter("#slideFrame_main>a:last");
+    })
+});
